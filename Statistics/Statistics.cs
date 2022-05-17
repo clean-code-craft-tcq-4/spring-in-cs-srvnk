462a1605b12e95378d8bbc5f1e6fe4d99fd788d0
srvnk <105205717+srvnk@users.noreply.github.com>
2022-05-17 18:57:50 +0530

    Update Statistics.cs
    Logic Implementation

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -7,6 +7,28 @@ namespace Statistics
     {
         public Stats CalculateStatistics(List<float> numbers) {
             //Implement statistics here
+            try
+            {
+                Stats retData = new Stats();
+                
+                if (numbers.Count() == 0)
+                {
+                    retData.average = Double.NaN;
+                    retData.min = Double.NaN;
+                    retData.max = Double.NaN; 
+                }
+                else
+                {
+                    retData.average = numbers.Average();
+                    retData.min = numbers.Min();
+                    retData.max = numbers.Max();
+                }
+                return retData;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in CalculateStatistics. " + ex.Message);
+            }                
         }
     }
 }
