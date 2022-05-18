22458009f328ddd63cbea30a3d48c668800b4b59
srvnk <105205717+srvnk@users.noreply.github.com>
2022-05-18 22:41:38 +0530

    Create StatsAlerter.cs

---
+++ Statistics/StatsAlerter.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace Statistics
+{
+    public class StatsAlerter
+    {
+        public float threshold;
+        public IAlerter[] alerter;
+        public StatsAlerter(float threshold, IAlerter[] alerter)
+        {
+           this.threshold = threshold;
+           this.alerter = alerter;
+        }
+
+        public void checkAndAlert(List<float> nums)
+        {
+            float maxValue = nums[0];
+            for (int i = 1; i < nums.Count; i++)
+            {
+                if (maxValue < nums[i])
+                {
+                    maxValue = nums[i];
+                }
+            }
+
+            if (maxValue > threshold)
+            {
+                alerter[0].alerter.GetType().GetProperty("emailAlert").SetValue(true, true);
+                alerter[1].alerter.GetType().GetProperty("ledGlows").SetValue(true, true);
+            }
+        }
+    }
+}
